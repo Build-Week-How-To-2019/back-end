d517f01db1ced802c69f575cfd5caaa19513bfd6
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-09-24 23:39:28 -0500

    db swithedc to sqlite

--- knexfile.js
+++ knexfile.js
@@ -34,17 +34,17 @@ module.exports = {
   //   },
   // },
 
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL, 
-    useNullAsDefault: true,
-    migrations: {
-      directory: './database/migrations',
-    },
-    seeds: {
-      directory: './database/seeds',
-    },
-  },
+  // production: {
+  //   client: 'pg',
+  //   connection: process.env.DATABASE_URL, 
+  //   useNullAsDefault: true,
+  //   migrations: {
+  //     directory: './database/migrations',
+  //   },
+  //   seeds: {
+  //     directory: './database/seeds',
+  //   },
+  // },
 
   testing: {
     client: 'sqlite3',
