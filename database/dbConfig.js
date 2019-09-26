c6921031fd246dc4455bdc76b145655482cf021f
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-09-26 16:20:33 -0500

    production db swithed to Postgres

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,8 +2,8 @@ const knex = require('knex');
 
 const config = require('../knexfile.js');
 
-const dbEnv = process.env.DB_ENV || 'development';
-//const dbEnv = process.env.NODE_ENV || 'development'; // if something else isn't setting ENV, use development
+//const dbEnv = process.env.DB_ENV || 'development';
+const dbEnv = process.env.NODE_ENV || 'development'; // if something else isn't setting ENV, use development
 
 module.exports = knex(config[dbEnv]);
 
