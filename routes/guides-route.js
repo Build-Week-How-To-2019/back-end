ae96b94bfa00e740ec9a5acfce22eddbe7d29c44
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-09-26 16:19:44 -0500

    production db swithed to Postgres

--- routes/guides-route.js
+++ routes/guides-route.js
@@ -22,6 +22,7 @@ router.post("/", (req, res) => {
    .catch((err) => {
      console.log(err)
      res.status(500).json({
+        err,
         message: 'Sorry, something went wrong while adding guide'
      })
    })
